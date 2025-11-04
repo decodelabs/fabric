055ba73e2ac2e901f4f87def8d9b9c3050121efb
Tom Wright <tom@inflatablecookie.com>
2025-11-04 12:38:59 +0000

    Improved doc comment headers
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Genesis/Build/Manifest.php
+++ src/Genesis/Build/Manifest.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @package Fabric
- * @license http://opensource.org/licenses/MIT
+ * Fabric
+ * @license https://opensource.org/licenses/MIT
  */
 
 declare(strict_types=1);
@@ -32,8 +32,8 @@ class Manifest implements ManifestInterface
             <?php
 
             /**
-             * @package Fabric
-             * @license http://opensource.org/licenses/MIT
+             * Fabric
+             * @license https://opensource.org/licenses/MIT
              */
 
             declare(strict_types=1);
